d8b69c845dcb2518d5dd2a55cee598ca64b3408c
SubconsciousEye <SubconsciousEye@protonmail.com>
2022-10-12 08:39:07 -0500

    Preliminary Lankario Stuff
    Just some of the base setup in preparation for the new Lankario/Weird_Mushroom powerup.
    TODO:
    * Proper Image Code (currently just copy+pasted from Bunny powerup)
    * Proper Main Code (doesn't really contain much right now)
    * Proper Tilemapping

---
+++ patch/powerup_files/powerups/subconsciouseye/lankario/lankario_main_code.asm
@@ -0,0 +1,9 @@
+;##################################################################################################
+;# Powerup: Lankario (AKA Lankio AKA Weird Mario)
+;# Author: Nintendo (Original), SubconsciousEye (Port)
+;# Description: The Weird Mushroom Powerup from Super Mario Maker.
+;#
+;# This code is run every frame when Mario has this powerup.
+    
+    bra $00
+    rts
---
+++ patch/powerup_files/powerups/subconsciouseye/lankario/lankario_powerdown_code.asm
@@ -0,0 +1,11 @@
+;##################################################################################################
+;# Powerup: Lankario (AKA Lankio AKA Weird Mario)
+;# Author: Nintendo (Original), SubconsciousEye (Port)
+;# Description: The Weird Mushroom Powerup from Super Mario Maker.
+;#
+;# This code will once run whenever Mario gets hurt while using this powerup.
+;#
+;# Note: In order to use this code, set the powerdown effect to Custom in the defs file.
+	
+    %powerdown_!{lankario_powerdown_action}(!lankario_powerdown_power_num, !lankario_powerdown_sfx_num, !lankario_powerdown_sfx_port)
+    rts
